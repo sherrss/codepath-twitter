948f0a89663b53d26d732bfd24eff98758cd71bb
sherrss <liu1500sherry@gmail.com>
2022-09-30 13:04:33 -0400

    Implement Twitter API Authentication

--- Twitter/TwitterAPICaller.swift
+++ Twitter/TwitterAPICaller.swift
@@ -10,7 +10,7 @@ import UIKit
 import BDBOAuth1Manager
 
 class TwitterAPICaller: BDBOAuth1SessionManager {    
-    static let client = TwitterAPICaller(baseURL: URL(string: "https://api.twitter.com"), consumerKey: "5lUJuO5AUpPUCez4ewYDFrtgh", consumerSecret: "s5ynGqXzstUZwFPxVyMDkYh197qvHOcVM3kwv1o2TKhS1avCdS")
+    static let client = TwitterAPICaller(baseURL: URL(string: "https://api.twitter.com"), consumerKey: "XYkFYU1cqmMjexwZSkBspnNqw", consumerSecret: "ehTRkd4oDtI2jg6JOik4f5N1eQv5HJqZIgdAMWaWFrXjmCsFPC")
     var loginSuccess: (() -> ())?
     var loginFailure: ((Error) -> ())?
     
